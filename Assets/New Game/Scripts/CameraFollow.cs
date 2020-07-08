e04fc187531a87eeb7a812e6161741935322995a
gabriel teles <gabrieltelessantos48@gmail.com>
2020-07-07 21:14:08 -0300

    Commit inicial

---
+++ Assets/New Game/Scripts/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+
+    void FixedUpdate()
+    {
+        Vector3 desiderPosition = target.position + offset;
+        Vector3 smootherdPosition = Vector3.Lerp(transform.position, desiderPosition, smoothSpeed);
+        transform.position = smootherdPosition;
+
+        transform.LookAt(target);
+    }
+}
